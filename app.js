b822293809665e5b777a854859859b7e62568640
Michał <130533795+MichalWachura@users.noreply.github.com>
2023-07-10 10:29:41 +0200

    Fix Footer in Media Queriess

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-//====================== CURSOR ====================== 
+//====================== CURSOR ======================
 let innerCursor = document.querySelector(".inner-cursor");
 let outerCursor = document.querySelector(".outer-cursor");
 
 document.addEventListener("mousemove", moveCursor);
 
 function moveCursor(e) {
-
 	let x = e.clientX;
 	let y = e.clientY;
 	// console.log(x, y);
 	innerCursor.style.left = `${x}px`;
-  innerCursor.style.top = `${y}px`;
-  outerCursor.style.left = `${x}px`;
-  outerCursor.style.top = `${y}px`;
-
+	innerCursor.style.top = `${y}px`;
+	outerCursor.style.left = `${x}px`;
+	outerCursor.style.top = `${y}px`;
 }
-//====================== DAY NIGHT ====================== 
+//====================== DAY NIGHT ======================
+
 const switchButton = document.getElementById("day-night");
+let mode = localStorage.getItem("mode");
 
+switchButton = document.addEventListener("click", changeDayNight);
 
-
-switchButton = document.addEventListener('click',changeDayNight);
-
-function changeDayNight(event){
-	
+function changeDayNight(event) {
 	const target = event.target;
-  if (target.id !== "day-night") return; // Exit if the click is not on the #day-night element
+	if (target.id !== "day-night") return; // Exit if the click is not on the #day-night element
 
-	document.querySelector("body").classList.toggle('dark-theme');
-	document.querySelector(".text-container").classList.toggle('dark-theme');
-	document.querySelector(".main-container").classList.toggle('dark-theme');
-	document.querySelector(".arrow-top").classList.toggle('dark-theme');
-	switchButton.classList.toggle('dark-theme');
-		if(switchButton.classList.contains('dark-theme')){
-				switchButton.innerHTML="+ light mode";
-			}else{
-				switchButton.innerHTML="+ dark mode";
-			}
-	};
+	document.querySelector("body").classList.toggle("dark-theme");
+	document.querySelector(".text-container").classList.toggle("dark-theme");
+	document.querySelector(".main-container").classList.toggle("dark-theme");
+	document.querySelector(".arrow-top").classList.toggle("dark-theme");
+	switchButton.classList.toggle("dark-theme");
+
+	if (switchButton.classList.contains("dark-theme")) {
+		switchButton.innerHTML = "+ light mode";
+		
+	} else {
+		switchButton.innerHTML = "+ dark mode";
+		
+	}
+};
+
+
+
+//  230709 
+//  Improve media queries  ---> footer padding,font size, 
+// working on dark mode (localStorage)
+
+
+
+
+
+
 
 
 
